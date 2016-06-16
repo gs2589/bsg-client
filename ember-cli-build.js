dc68eb2fe355fb0fd7b568bba1cd62471ca340e8
dunxtand <mdrstandish@gmail.com>
2016-06-16 12:36:27 -0400

    implemented basic theme files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,34 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import("vendor/css/bootstrap.min.css");
+  app.import("vendor/css/font-awesome.min.css");
+  app.import("vendor/css/jquery.gritter.css");
+  app.import("vendor/css/less-style.css");
+  app.import("vendor/css/style.css");
+
+  app.import("vendor/js/jquery.js");
+  app.import("vendor/js/bootstrap.min.js");
+  app.import("vendor/js/jquery-ui.min.js");
+  app.import("vendor/js/bootstrap-datetimepicker.min.js");
+  app.import("vendor/js/wysihtml5-0.3.0.js");
+  app.import("vendor/js/prettify.js");
+  app.import("vendor/js/bootstrap-wysihtml5.min.js");
+  app.import("vendor/js/jquery.validate.js");
+  app.import("vendor/js/jquery.steps.min.js");
+  app.import("vendor/js/jquery.knob.js");
+  app.import("vendor/js/jquery.slimscroll.min.js");
+  app.import("vendor/js/jquery.dataTables.min.js");
+  app.import("vendor/js/jquery.prettyPhoto.js");
+  app.import("vendor/js/jquery.rateit.min.js");
+  app.import("vendor/js/moment.min.js");
+  app.import("vendor/js/fullcalendar.min.js");
+  app.import("vendor/js/jquery.gritter.min.js");
+  app.import("vendor/js/custom.notification.js");
+  app.import("vendor/js/respond.min.js");
+  app.import("vendor/js/html5shiv.js");
+  app.import("vendor/js/custom.js");
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //

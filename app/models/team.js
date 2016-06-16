8d325ec8e412dc140f76205d5a95c1ccbafa5290
gs2589 <gabriel.seidner@gmail.com>
2016-06-16 10:17:18 -0400

    displayed cyclists on index with array objects for relationships

--- app/models/team.js
+++ app/models/team.js
@@ -2,12 +2,12 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 
+
 export default Model.extend({
 
   name: attr('string'),
-  email:attr('string'),
-  teams: hasMany('team'),
+  cyclists: hasMany('cyclist'),
   rides: hasMany('ride')
- 
+
 });
   

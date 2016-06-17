6e04236d75eabe0a70d33c14f8a9f4454801cf1c
gs2589 <gabriel.seidner@gmail.com>
2016-06-17 15:49:08 -0400

    implemented code for authentication but not tested

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,10 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';  
+import config from '../config/environment';
+
 
 export default JSONAPIAdapter.extend({
   host: 'http://localhost:3000',
-  namespace: 'api/v1'
+  namespace: 'api/v1',
+  authorizer: 'authorizer:custom'
 });
---
+++ app/routes/application.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+
+const {service} = Ember.inject;
+
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  
+  //session: service('session'),
+  
+
+  actions: {
+      invalidateSession: function() {
+         this.get('session').invalidate();
+      }
+  }
+
+});

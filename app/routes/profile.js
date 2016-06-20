692478676978faaafde7237e838c611d1c65ed44
dunxtand <mdrstandish@gmail.com>
2016-06-20 09:17:17 -0400

    modify profile.js to be more descriptive

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     let currentCyclist = this.store.queryRecordPath('cyclist', 'current');
-    debugger;
     return currentCyclist;
   }
 });

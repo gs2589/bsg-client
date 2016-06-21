d50b50c5861ee1c6274c15bf1c68d7369c6f2edb
dunxtand <mdrstandish@gmail.com>
2016-06-20 21:28:13 -0400

    add dataAdapterMixin back into adapters/application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';  
 import config from '../config/environment';
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: `${config.host}`,
   namespace: 'api/v1',
   authorizer: 'authorizer:custom'
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isLoggedIn: isAuthInfoInLocalStorage(),
 
   session: Ember.inject.service(),
+
+  isLoggedIn: isAuthInfoInLocalStorage(),
 
   actions: {
     authenticate: function(authenticator, credentials) {
